27235269ff2d95d633a62e3b6883189cb055e0cf
Hao Luo <howlowck@gmail.com>
2022-12-19 04:56:42 -0600

    fix pubsub connection string

--- server/index.ts
+++ server/index.ts
@@ -8,7 +8,7 @@ const port = process.env.port ?? process.env.PORT ?? 8585
 const app = makeApp({
   environment: (process.env.NODE_ENV as 'prod' | 'dev' | 'test') ?? 'prod',
   dataStorageType: (process.env.DATA_STORAGE_TYPE as 'aztable') ?? 'aztable',
-  pubsubConnectionString: process.env.PUBSUB_CONNECTION_STRING!,
+  pubsubConnectionString: process.env.AZ_PUBSUB_CONNECTION_STRING!,
   pubsubHub: appName,
   dataStorageConnectionString: process.env.DATA_STORAGE_CONNECTION_STRING,
 })
